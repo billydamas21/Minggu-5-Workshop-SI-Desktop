e33dfef173efc7fa7aa39f2977fcdd55230bc96f
billydamas21 <68609349+billydamas21@users.noreply.github.com>
2021-04-11 20:37:29 +0700

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,17 @@
+package binary_tree;
+
+/**
+ *
+ * @author Amalia
+ */
+
+public class Node {
+    public int id;
+    public String data;
+    public Node leftChild;
+    public Node rightChild;
+    
+    public void displayNode(){
+        System.out.print(" { " + id + " " + data + " } ");
+    }
+}  
---
+++ Vertex.java
@@ -0,0 +1,15 @@
+package graph;
+
+/**
+ *
+ * @author Amalia
+ */
+
+public class Vertex {
+    public char label;
+    public boolean wasVisited;
+    
+    public Vertex(char label) {
+        this.label = label; wasVisited = false;
+    }
+}
